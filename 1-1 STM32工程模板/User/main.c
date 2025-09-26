ad89d18e491b17de8d08796e62d38f5b761c771e
user-arthur <2445338794@qq.com>
2025-09-26 15:01:38 +0800

    尝试发送和接收

--- 1-1 STM32工程模板/User/main.c
+++ 1-1 STM32工程模板/User/main.c
@@ -1,29 +1,34 @@
 #include "stm32f10x.h"                  // Device header
+#include "usart.h"
+
+
+uint16_t i;
+
 
 int main(void)
 {
-	/*开启时钟*/
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);	//开启GPIOC的时钟
-															//使用各个外设前必须开启时钟，否则对外设的操作无效
-	
-	/*GPIO初始化*/
-	GPIO_InitTypeDef GPIO_InitStructure;					//定义结构体变量
-	
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;		//GPIO模式，赋值为推挽输出模式
-	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;				//GPIO引脚，赋值为第13号引脚
-	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;		//GPIO速度，赋值为50MHz
-	
-	GPIO_Init(GPIOC, &GPIO_InitStructure);					//将赋值后的构体变量传递给GPIO_Init函数
-															//函数内部会自动根据结构体的参数配置相应寄存器
-															//实现GPIOC的初始化
-	
-	/*设置GPIO引脚的高低电平*/
-	/*若不设置GPIO引脚的电平，则在GPIO初始化为推挽输出后，指定引脚默认输出低电平*/
-//	GPIO_SetBits(GPIOC, GPIO_Pin_13);						//将PC13引脚设置为高电平
-	GPIO_ResetBits(GPIOC, GPIO_Pin_13);						//将PC13引脚设置为低电平
+	USART1_Init(921600);
+	U0_printf("%d,%c,%x\r\n",0x30,0x30,0x30);
 	
 	while (1)
 	{
-		
+		if(U0CB.URxDataOUT != U0CB.URxDataIN)
+		{
+			U0_printf("本次接收了%d字节数据\r\n",U0CB.URxDataOUT->end - U0CB.URxDataOUT->start + 1);
+			for(i = 0;i<U0CB.URxDataOUT->end - U0CB.URxDataOUT->start + 1;i++)
+			{
+				U0_printf("%c ",U0CB.URxDataOUT->start[i]);
+			}
+
+			U0_printf("\r\n\r\n");
+			
+			U0CB.URxDataOUT ++;
+			if(U0CB.URxDataOUT == U0CB.URxDataEND)
+			{
+				U0CB.URxDataOUT = &U0CB.URxDataPtr[0];
+			}
+
+
+		}
 	}
 }
