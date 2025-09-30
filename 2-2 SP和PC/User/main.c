8ffc9a91fca5273084fcaac5d8f25766b8fdd47d
user-arthur <2445338794@qq.com>
2025-09-30 09:02:30 +0800

    SP和PC设置并测试

---
+++ 2-2 SP和PC/User/main.c
@@ -0,0 +1,27 @@
+#include "stm32f10x.h"                  // Device header
+#include "usart.h"
+#include "delay.h"
+#include "AT24C02.h"
+#include "W25Q64.h"
+#include "main.h"
+#include "boot.h"
+
+
+
+OTA_InfoCB OTA_Info;
+int main(void)
+{	
+
+
+	Delay_Init();
+	USART1_Init(115200);
+		printf("heloo");
+	AT24C02_Init();
+	AT24C02_ReadOTAInfo();
+	BootLoader_Brance();
+
+	while (1)
+	{
+
+	}
+}
